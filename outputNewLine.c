5a622891cb8e97ac847c5473c44709a6bf7ecace
anji <aveeranki@nouveau-labs.com>
2023-10-31 20:53:59 +0530

    added multinew line

---
+++ outputNewLine.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hiiii Potttiii\n how are you ???\n em chesthunnvvvv \t Miss youu");
+    return 0;
+}
+
+/*
+OUTPUT:--
+    Hiiii Potttiii
+    how are you ???
+    em chesthunnvvvv        Miss youu
+
+*/
+
+/*
+
+       The newline character (\n) is called an escape sequence, and it forces the cursor to 
+    change its position to the beginning of the next line on the screen.This results in a new line.
+
+
+     ==> Examples of other valid escape sequences are:--
+        \t   -----> Create a Tab
+        \\ -------> Inserts a backslash (\)
+        \" -------> Inserts a double quotes (")
+
+*/
