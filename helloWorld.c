c3ce9c98158b410b4535a7dbdb37cae337b41ea7
anji <aveeranki@nouveau-labs.com>
2023-11-01 08:53:11 +0530

    changed void to int

--- helloWorld.c
+++ helloWorld.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-int main(){
+void main(){
     printf("Hellooo world\n");
     printf("Hellloo Pottiiiiii");
-    return 0;
 }
 
 /*
- Line 1: #include <stdio.h> is a header file library that lets us work with input and output functions, such as printf() (used in line 4). Header files add functionality to C programs.
+ Line 1: #include <stdio.h> is a header file library that lets us work with input and output functions, 
+         such as printf(),scanf() (used in line 4). Header files add functionality to C programs.
  Line 2: A blank line. C ignores white space. But we use it to make the code more readable.
- Line 3: Another thing that always appear in a C program, is main(). This is called a function. Any code inside its curly brackets {} will be executed.
+ Line 3: Another thing that always appear in a C program, is main(). This is called a function. 
+          Any code inside its curly brackets {} will be executed.
  Line 4: printf() is a function used to output/print text to the screen. In our example it will output "Hello World!".
 
     
@@ -22,4 +23,5 @@ int main(){
     
  Line 5: return 0 ends the main() function.
  Line 6: Do not forget to add the closing curly bracket } to actually end the main function.
+
 */
