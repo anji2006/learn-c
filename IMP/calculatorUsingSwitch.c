9b4847c3afc1994bc09bff2e3a84a02e6d3f7954
anji <aveeranki@nouveau-labs.com>
2024-01-27 16:04:40 +0530

    calculator

---
+++ IMP/calculatorUsingSwitch.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    char operation;
+    float num1, num2, result;
+
+    // Input operation from the user
+    printf("Enter an operation (+, -, *, /): ");
+    scanf(" %c", &operation);
+
+    // Input two numbers from the user
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    // Perform the operation based on the user's choice
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2f + %.2f = %.2f\n", num1, num2, result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2f - %.2f = %.2f\n", num1, num2, result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2f * %.2f = %.2f\n", num1, num2, result);
+            break;
+        case '/':
+            result = num1 / num2;
+            printf("Result: %.2f / %.2f = %.2f\n", num1, num2, result);
+        
+            break;
+        default:
+            printf("Invalid operation!\n");
+    }
+
+    return 0;
+}
